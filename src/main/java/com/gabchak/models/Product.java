f48bbfd47e2cefc315fe57b3ffb4f2b2d19ad087
gabchak <andrey.gabchak@gmail.com>
2019-03-25 18:23:31 +0200

    Moved url to product details. Removed constructor annotation. Added method addProductDetails.

--- src/main/java/com/gabchak/models/Product.java
+++ src/main/java/com/gabchak/models/Product.java
@@ -1,19 +1,28 @@
 package com.gabchak.models;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Getter
 @Setter
-@AllArgsConstructor
 public class Product {
 
     private String name;
     private String brand;
-    private String url;
 
-    private Set<ProductDetails> productDetails;
+    private Set<ProductDetail> productDetails;
+
+    public void addProductDetails(ProductDetail productDetail) {
+        if (this.productDetails == null) {
+            this.productDetails = new HashSet<>();
+        }
+        this.productDetails.add(productDetail);
+    }
+
+    public static Product getInstance() {
+        return new Product();
+    }
 }
