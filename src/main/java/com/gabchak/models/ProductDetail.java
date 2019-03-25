e50d2f500a6e35a48a771136a0d4492f5d5a77a7
gabchak <andrey.gabchak@gmail.com>
2019-03-25 18:24:42 +0200

    Moved url to product details. Renamed to singular form. Added fabric method. Equals and hashcode

---
+++ src/main/java/com/gabchak/models/ProductDetail.java
@@ -0,0 +1,26 @@
+package com.gabchak.models;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.jsoup.nodes.Document;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@EqualsAndHashCode(of = "url")
+public class ProductDetail {
+
+    private String url;
+    private Document htmlDocument;
+    private String color;
+    private String price;
+    private String articleId;
+    private Set<String> sizeSet;
+
+    public static ProductDetail getInstance() {
+        return new ProductDetail();
+    }
+
+}
